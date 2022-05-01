456f059ab12f6d9970f2b49190e83fc0fe57f76a
Gathers <gathers@gmail.com>
2022-05-01 13:11:16 +0200

    Add first version of script and description.
    Signed-off-by: Gathers <gathers@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # BWAIShotgunRunner
-Script and description for running BWAIShotgun in Windows Sandbox.
+Script and description for running BWAIShotgun in a Windows Sandbox.
+
+1. Install Windows Sandbox, see https://docs.microsoft.com/en-us/windows/security/threat-protection/windows-sandbox/windows-sandbox-overview
+2. Open a new Windows Sandbox.
+3. Copy the folder "shotgun" to the desktop. (the folder, not just the contents)
+4. Right-click "setup.ps1" and "Run with PowerShell".
+
+The script will download Starcraft and BWAIShotgun, install dependencies and start a bot match.
+
+TODO: Install Java. Perhaps add way to download bots from SSCAIT.
---
+++ shotgun/setup.ps1
@@ -0,0 +1,46 @@
+#download and check starcraft.zip
+if (!(Test-Path starcraft.zip)) {
+    Invoke-WebRequest 'http://files.theabyss.ru/sc/starcraft.zip' -OutFile starcraft.zip
+    if (-not (Get-FileHash starcraft.zip -Algorithm SHA256).Hash -eq (Get-Content starcraft.zip.sha256).Substring(0, 64)) {
+	    throw "Wrong hash for starcraft.zip file."
+    }
+}
+
+#extract starcraft
+if (!(Test-Path starcraft.exe)) {
+    Expand-Archive -Path starcraft.zip -DestinationPath . -Force
+}
+
+#Install 7zip module needed to extract BWAIShotgun
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Set-PSRepository -Name 'PSGallery' -SourceLocation "https://www.powershellgallery.com/api/v2" -InstallationPolicy Trusted
+Install-Module -Name 7Zip4Powershell -Force
+#-SkipPublisherCheck
+
+#download and install VC2015 runtime (x64 for BWAIShotgun)
+if (!(Test-Path VC_redist.x64.exe)) {
+    Invoke-WebRequest 'https://aka.ms/vs/17/release/vc_redist.x64.exe' -OutFile VC_redist.x64.exe
+}
+Start-Process -FilePath VC_redist.x64.exe -ArgumentList "/passive" -Wait -Passthru
+
+#download and install VC2015 runtime (x86 for bot ZergHell)
+if (!(Test-Path VC_redist.x86.exe)) {
+    Invoke-WebRequest 'https://aka.ms/vs/17/release/vc_redist.x86.exe' -OutFile VC_redist.x86.exe
+}
+Start-Process -FilePath VC_redist.x86.exe -ArgumentList "/passive" -Wait -Passthru
+
+#download BWAIShotgun
+if (!(Test-Path bwaishotgun.7z)) {
+    Invoke-WebRequest 'https://github.com/Bytekeeper/BWAIShotgun/releases/download/v0.4/bwaishotgun.7z' -OutFile bwaishotgun.7z
+}
+
+#extract BWAIShotgun
+if (!(Test-Path bwaishotgun.exe)) {
+    Expand-7Zip -ArchiveFileName bwaishotgun.7z -TargetPath . -Password shotgun
+}
+
+Copy-Item _game.toml game.toml
+Copy-Item _shotgun.toml shotgun.toml
+
+Start-Process -FilePath BWAIShotgun.exe -Wait -Passthru
